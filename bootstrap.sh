ee4fa648bce4f6a8329633899650b0fd1c964597
Arsany Samuel <arsanysamuel.as@gmail.com>
2023-02-23 14:25:58 +0200

    gpg check skip

--- bootstrap.sh
+++ bootstrap.sh
@@ -156,7 +156,7 @@ deploydotfiles() {
     printf "\tConfiguring SSH...\n"
     cd $homedir
     yes | sudo -u $username ssh-keygen -q -N "" -C "" -t rsa -f $homedir/.ssh/id_rsa
-    yes | sudo -u $username ssh-keygen -q -N "" -C "" -t e25519 -f $homedir/.ssh/id_e25519
+    yes | sudo -u $username ssh-keygen -q -N "" -C "" -t ed25519 -f $homedir/.ssh/id_ed25519
     sudo -u $username ssh-keyscan github.com >> $homedir/.ssh/known_hosts 2> /dev/null
 
     printf "\tDeploying dotfiles...\n"
@@ -174,8 +174,8 @@ installpkglist() {
 
     pkglist=$(cat "$homedir/.config/pkglist.txt")
     for pkg in $pkglist; do
-        printf "$pkg... "
-        sudo -u $username pikaur -S --noconfirm --needed $pkg > /dev/null 2>&1 || return 1
+        printf "\t$pkg... "
+        yes p | sudo -u $username pikaur -S --noconfirm --needed $pkg > /dev/null 2>&1 || return 1
         printf "done.\n"
     done
 
